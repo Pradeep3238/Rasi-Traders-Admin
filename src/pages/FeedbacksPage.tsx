12f34dd20b90cc7ac58da38d28b66a326a7d20f5
Pradeep3238 <d.pradeep3238@gmail.com>
2024-05-07 23:09:16 +0530

    added feedbacks page

--- src/pages/FeedbacksPage.tsx
+++ src/pages/FeedbacksPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import  { useEffect, useState } from 'react';
+import {  Table } from 'antd';
 import type { TableProps } from 'antd';
 
 interface DataType {
@@ -12,77 +12,44 @@ interface DataType {
 
 const columns: TableProps<DataType>['columns'] = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: 'email',
+    dataIndex: 'email',
+    key: 'email',
     render: (text) => <a>{text}</a>,
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
+    title: 'Feedback',
+    dataIndex: 'feedback',
+    key: 'feedback',
   },
 ];
 
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
 
-const FeedbacksPage: React.FC = () => <Table columns={columns} dataSource={data} />;
+const FeedbacksPage: React.FC = () =>{
+  const [data, setData] = useState();
 
+  useEffect(() => {
+    const fetchFeedbacks= async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/feedback`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch Feedbacks");
+        }
+        const res = await response.json();
+        setData(res.data);
+      } catch (error) {
+        console.log("Error fetching Users:", error);
+      }
+    };
+
+    fetchFeedbacks();
+  }, []);
+
+  return(
+     <Table columns={columns} dataSource={data} />
+  )
+}
 export default FeedbacksPage;
